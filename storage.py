d208c565e41ed76db563d2ff76ce7921ee10dd6b
Chris <72231921+cfugsss@users.noreply.github.com>
2022-02-24 23:26:26 -0500

    Add files via upload

---
+++ storage.py
@@ -0,0 +1,18 @@
+# list stored in username - site - password
+
+theVault = []
+
+def vaultFunc(x, y , z):
+    [theVault.append(x), theVault.append(y), theVault.append(z)]
+    return theVault
+
+realVault = theVault
+print (realVault)
+
+
+
+#from list_program import list_function       dont write .py for the script   
+                                              #name and dont use () for 
+                                              #the function name
+
+#list_that_you_want = list_function(var)
